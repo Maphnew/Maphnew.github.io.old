4bcec1a4b03eb26ae1abb86ff2a8800e1e5d9a2a
Sung Chan <33482265+Maphnew@users.noreply.github.com>
2019-03-04 22:54:17 +0900

    Update clock.js

--- VanillaJS/clock.js
+++ VanillaJS/clock.js
@@ -10,14 +10,14 @@ function getTime(){
     clockTitle.innerText = `${
         hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds}.${
-        milliseconds < 10 ? `00${milliseconds}` : milliseconds < 100 ? `0${milliseconds}`: milliseconds
-        }`;
+        seconds < 10 ? `0${seconds}` : seconds} 
+        // .${milliseconds < 10 ? `00${milliseconds}` : milliseconds < 100 ? `0${milliseconds}`: milliseconds}
+        `;
 }
 
 function init() {
     getTime();
-    setInterval(getTime, 100);
+    setInterval(getTime, 1000);
 }
 
 init();
